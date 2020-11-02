d198119af5b8c16fdd7a6e41421524c98f2a677e
vipul <vipultagore@gmail.com>
2020-11-02 20:58:13 +0530

    about some done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(document).ready(function()){
-    $(window).scroll(function()){
-        if(this.scrollY>20){
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }
         else{
             $('.navbar').removeClass("sticky");
         }
-    }
-}
+    })
+});
