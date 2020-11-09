564b4a4f5f943b19f57af57351060fd347d77bbc
vipul <vipultagore@gmail.com>
2020-11-09 21:21:15 +0530

    complete

--- script.js
+++ script.js
@@ -6,6 +6,29 @@ $(document).ready(function(){
         else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    // Typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Web Developer","Design Thinker"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-1",{
+        strings: ["Web Developer","Design Thinker"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
     $('.carousel').owlCarousel({
         margin: 20,
