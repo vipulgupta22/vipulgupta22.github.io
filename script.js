5e95d7486cd04f895ceae4a16fdb9cbef8e237b5
vipul <vipultagore@gmail.com>
2021-02-11 19:19:25 +0530

    done

--- script.js
+++ script.js
@@ -13,41 +13,4 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    // slide up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-    });
-    // Typing animation script
-    var typed = new Typed(".typing",{
-        strings: ["Web Developer","Design Thinker"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-1",{
-        strings: ["Web Developer","Design Thinker"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
 });
